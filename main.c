a90a567c3d8ca9806e3d4ffecb12b8ea48373671
Bogdan Guliaev <bgs99c@gmail.com>
2018-11-03 20:35:12 +0300

    Implemented autocompletion of function names(including constructors)

--- main.c
+++ main.c
@@ -5,6 +5,36 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static dict *ad = NULL;
+static const char **names;
+char * function_name_generator(const char *text, int state);
+
+char **function_completion(const char *text, int start, int end){
+    rl_attempted_completion_over = 1;
+    end = start;
+    return rl_completion_matches(text, function_name_generator);
+}
+
+
+char *function_name_generator(const char *text, int state){
+
+    static unsigned long list_index, len;
+    const char *name;
+
+    if (!state) {
+        list_index = 0;
+        len = strlen(text);
+    }
+
+    while ((name = names[list_index++])) {
+        if (strncmp(name, text, len) == 0) {
+            return strdup(name);
+        }
+    }
+
+    return NULL;
+}
+
 int main(int argc, char **argv)
 {
     const char *path = "input.shs";
@@ -22,7 +52,20 @@ int main(int argc, char **argv)
     fread(all, 1, len, in);
     fclose(in);
 
-    const dict *ad = process_all(all);
+    ad = process_all(all);
+
+    unsigned long adl = 0;
+    for(dict *td = ad; td; td = td->next)
+        adl++;
+    names = calloc(adl+1, sizeof (char *));
+    int i = 0;
+    for(dict *td = ad; td; td = td->next, i++){
+        names[i] = td->val->val->t->f->name;
+    }
+
+
+    rl_attempted_completion_function = function_completion;
+
     printf("%s \n", all);
 
     char *ui;
